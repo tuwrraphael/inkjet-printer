7a71ea5025f57639f92ae0ee83816fca86c0d142
tuwrraphael <raphael.hauk@racing.tuwien.ac.at>
2024-01-31 14:29:31 +0100

    pinout tested

--- inkjet-printer-zephyr/inkjet-printer/app/src/main.c
+++ inkjet-printer-zephyr/inkjet-printer/app/src/main.c
@@ -9,51 +9,517 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 #include <zephyr/logging/log.h>
-
-
+#include <zephyr/shell/shell.h>
+#include <zephyr/console/console.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
-#define SW0_NODE	DT_ALIAS(sw0)
+#define SW0_NODE DT_ALIAS(sw0)
 #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
 
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
+															  {0});
 static struct gpio_callback button_cb_data;
 
+static const struct gpio_dt_spec vpp_en = GPIO_DT_SPEC_GET(DT_NODELABEL(vpp_en), gpios);
+static const struct gpio_dt_spec comm_enable = GPIO_DT_SPEC_GET(DT_NODELABEL(comm_enable), gpios);
+static const struct gpio_dt_spec n_reset_mcu = GPIO_DT_SPEC_GET(DT_NODELABEL(n_reset_mcu), gpios);
+static const struct gpio_dt_spec vpp_enable_cp = GPIO_DT_SPEC_GET(DT_NODELABEL(vpp_enable_cp), gpios);
+static const struct gpio_dt_spec reset_disable_cp = GPIO_DT_SPEC_GET(DT_NODELABEL(reset_disable_cp), gpios);
+
+static const struct gpio_dt_spec n_reset_fault = GPIO_DT_SPEC_GET(DT_NODELABEL(n_reset_fault), gpios);
+static const struct gpio_dt_spec n_reset_in = GPIO_DT_SPEC_GET(DT_NODELABEL(n_reset_in), gpios);
+
+static struct gpio_dt_spec nSS2 = GPIO_DT_SPEC_GET(DT_NODELABEL(nss2), gpios);
+static struct gpio_dt_spec nSS1 = GPIO_DT_SPEC_GET(DT_NODELABEL(nss1), gpios);
+static struct gpio_dt_spec PHO = GPIO_DT_SPEC_GET(DT_NODELABEL(pho), gpios);
+
+static struct gpio_dt_spec nFAULT = GPIO_DT_SPEC_GET(DT_NODELABEL(nfault), gpios);
+static struct gpio_dt_spec READY = GPIO_DT_SPEC_GET(DT_NODELABEL(ready), gpios);
+
+static struct gpio_callback n_reset_fault_cb_data;
+static struct gpio_callback n_reset_in_cb_data;
+
+static struct gpio_callback nFAULT_cb_data;
+static struct gpio_callback READY_cb_data;
+
+K_EVENT_DEFINE(btn_event);
+
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+					uint32_t pins)
 {
-	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
-	const struct device *fire_dev;
-	fire_dev = DEVICE_DT_GET(DT_NODELABEL(printer_fire));
-	printer_fire(fire_dev);
+	// printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	// const struct device *fire_dev;
+	// fire_dev = DEVICE_DT_GET(DT_NODELABEL(printer_fire));
+	// // printer_fire(fire_dev);
+	k_event_post(&btn_event, 0x1);
 }
+
+void n_reset_fault_changed(const struct device *dev, struct gpio_callback *cb,
+						   uint32_t pins)
+{
+	LOG_INF("n_reset_fault changed to %d", gpio_pin_get_dt(&n_reset_fault));
+}
+
+void n_reset_in_changed(const struct device *dev, struct gpio_callback *cb,
+						uint32_t pins)
+{
+	LOG_INF("n_reset_in changed to %d", gpio_pin_get_dt(&n_reset_in));
+}
+
+void nFault_int_handler(const struct device *dev, struct gpio_callback *cb,
+						  uint32_t pins)
+{
+	LOG_INF("nFault changed to %d", gpio_pin_get_dt(&nFAULT));
+}
+
+void READY_int_handler(const struct device *dev, struct gpio_callback *cb,
+						 uint32_t pins)
+{
+	LOG_INF("READY changed to %d", gpio_pin_get_dt(&READY));
+}
+
+static int printhead_io_init()
+{
+	int ret;
+	if (!gpio_is_ready_dt(&nSS2))
+	{
+		LOG_ERR("Error: nSS2 device %s is not ready\n",
+				nSS2.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&nSS2, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, nSS2.port->name, nSS2.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&nSS1))
+	{
+		LOG_ERR("Error: nSS1 device %s is not ready\n",
+				nSS1.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&nSS1, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, nSS1.port->name, nSS1.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&PHO))
+	{
+		LOG_ERR("Error: PHO device %s is not ready\n",
+				PHO.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&PHO, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, PHO.port->name, PHO.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&nFAULT))
+	{
+		LOG_ERR("Error: nFAULT device %s is not ready\n",
+				nFAULT.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&nFAULT, GPIO_INPUT);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, nFAULT.port->name, nFAULT.pin);
+		return ret;
+	}
+	ret = gpio_pin_interrupt_configure_dt(&nFAULT,
+										  GPIO_INT_EDGE_BOTH);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n",
+				ret, nFAULT.port->name, nFAULT.pin);
+		return ret;
+	}
+	gpio_init_callback(&nFAULT_cb_data, nFault_int_handler, BIT(nFAULT.pin));
+	ret = gpio_add_callback(nFAULT.port, &nFAULT_cb_data);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to add callback on %s pin %d\n",
+				ret, nFAULT.port->name, nFAULT.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&READY))
+	{
+		LOG_ERR("Error: READY device %s is not ready\n",
+				READY.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&READY, GPIO_INPUT);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, READY.port->name, READY.pin);
+		return ret;
+	}
+	ret = gpio_pin_interrupt_configure_dt(&READY,
+										  GPIO_INT_EDGE_BOTH);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n",
+				ret, READY.port->name, READY.pin);
+		return ret;
+	}
+	gpio_init_callback(&READY_cb_data, READY_int_handler, BIT(READY.pin));
+	ret = gpio_add_callback(READY.port, &READY_cb_data);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to add callback on %s pin %d\n",
+				ret, READY.port->name, READY.pin);
+		return ret;
+	}
+	return 0;
+}
+
+int initialize_io()
+{
+	int ret;
+	ret = printhead_io_init();
+	if (ret != 0)
+	{
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&vpp_en))
+	{
+		LOG_ERR("Error: vpp_en device %s is not ready\n",
+				vpp_en.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&vpp_en, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, vpp_en.port->name, vpp_en.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&comm_enable))
+	{
+		LOG_ERR("Error: comm_enable device %s is not ready\n",
+				comm_enable.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&comm_enable, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, comm_enable.port->name, comm_enable.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&n_reset_mcu))
+	{
+		LOG_ERR("Error: n_reset_mcu device %s is not ready\n",
+				n_reset_mcu.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&n_reset_mcu, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, n_reset_mcu.port->name, n_reset_mcu.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&vpp_enable_cp))
+	{
+		LOG_ERR("Error: vpp_enable_cp device %s is not ready\n",
+				vpp_enable_cp.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&vpp_enable_cp, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, vpp_enable_cp.port->name, vpp_enable_cp.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&reset_disable_cp))
+	{
+		LOG_ERR("Error: reset_disable_cp device %s is not ready\n",
+				reset_disable_cp.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&reset_disable_cp, GPIO_OUTPUT_INACTIVE);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, reset_disable_cp.port->name, reset_disable_cp.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&n_reset_fault))
+	{
+		LOG_ERR("Error: n_reset_fault device %s is not ready\n",
+				n_reset_fault.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&n_reset_fault, GPIO_INPUT);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, n_reset_fault.port->name, n_reset_fault.pin);
+		return ret;
+	}
+	ret = gpio_pin_interrupt_configure_dt(&n_reset_fault,
+										  GPIO_INT_EDGE_BOTH);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n",
+				ret, n_reset_fault.port->name, n_reset_fault.pin);
+		return ret;
+	}
+	gpio_init_callback(&n_reset_fault_cb_data, n_reset_fault_changed, BIT(n_reset_fault.pin));
+	ret = gpio_add_callback(n_reset_fault.port, &n_reset_fault_cb_data);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to add callback on %s pin %d\n",
+				ret, n_reset_fault.port->name, n_reset_fault.pin);
+		return ret;
+	}
+	if (!gpio_is_ready_dt(&n_reset_in))
+	{
+		LOG_ERR("Error: n_reset_in device %s is not ready\n",
+				n_reset_in.port->name);
+		return ENODEV;
+	}
+	ret = gpio_pin_configure_dt(&n_reset_in, GPIO_INPUT);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				ret, n_reset_in.port->name, n_reset_in.pin);
+		return ret;
+	}
+	ret = gpio_pin_interrupt_configure_dt(&n_reset_in,
+										  GPIO_INT_EDGE_BOTH);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to configure interrupt on %s pin %d\n",
+				ret, n_reset_in.port->name, n_reset_in.pin);
+		return ret;
+	}
+	gpio_init_callback(&n_reset_in_cb_data, n_reset_in_changed, BIT(n_reset_in.pin));
+	ret = gpio_add_callback(n_reset_in.port, &n_reset_in_cb_data);
+	if (ret != 0)
+	{
+		LOG_ERR("Error %d: failed to add callback on %s pin %d\n",
+				ret, n_reset_in.port->name, n_reset_in.pin);
+		return ret;
+	}
+	return 0;
+}
+
+static int shell_wait(const struct shell *sh)
+{
+	int32_t events;
+	shell_print(sh, "Press button to continue.");
+	k_event_clear(&btn_event, 0x1);
+	events = k_event_wait_all(&btn_event, 0x1, false, K_SECONDS(20));
+	if (events == 0)
+	{
+		shell_print(sh, "Timeout");
+		return ETIMEDOUT;
+	}
+	return 0;
+}
+
+static int cmd_test_hv_supply(const struct shell *sh, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	shell_print(sh, "Disconnect HV supply, discharge capacitor, confirm 0V on printhead connector VPPH and VPPL.");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	shell_print(sh, "Connect HV supply, confirm 0V on printhead connector VPPH and VPPL.");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	gpio_pin_set_dt(&vpp_en, 1);
+	shell_print(sh, "Confirm again 0V on VPPH and VPPL.");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	gpio_pin_set_dt(&vpp_enable_cp, 1);
+	k_sleep(K_MSEC(1));
+	gpio_pin_set_dt(&vpp_enable_cp, 0);
+	shell_print(sh, "Confirm VPPH and VPPL active.");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	gpio_pin_set_dt(&vpp_en, 0);
+	shell_print(sh, "Confirm again 0V on VPPH and VPPL.");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	shell_print(sh, "done");
+	return 0;
+}
+
+static int cmd_test_printhead_io(const struct shell *sh, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	gpio_pin_set_dt(&nSS2, 0);
+	gpio_pin_set_dt(&nSS1, 0);
+	gpio_pin_set_dt(&PHO, 0);
+	shell_print(sh, "Connect nSS2 to READY");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	if (gpio_pin_get_dt(&READY) != 0)
+	{
+		shell_print(sh, "READY is not low");
+		return EINVAL;
+	}
+	gpio_pin_set_dt(&nSS2, 1);
+	k_sleep(K_MSEC(1));
+	if (gpio_pin_get_dt(&READY) != 1)
+	{
+		shell_print(sh, "READY is not high");
+		return EINVAL;
+	}
+	gpio_pin_set_dt(&nSS2, 0);
+	shell_print(sh, "Connect nSS1 to READY");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	if (gpio_pin_get_dt(&READY) != 0)
+	{
+		shell_print(sh, "READY is not low");
+		return EINVAL;
+	}
+	gpio_pin_set_dt(&nSS1, 1);
+	k_sleep(K_MSEC(1));
+	if (gpio_pin_get_dt(&READY) != 1)
+	{
+		shell_print(sh, "READY is not high");
+		return EINVAL;
+	}
+	gpio_pin_set_dt(&nSS1, 0);
+	shell_print(sh, "Connect PHO to nFAULT");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return ETIMEDOUT;
+	}
+	if (gpio_pin_get_dt(&nFAULT) != 0)
+	{
+		shell_print(sh, "nFAULT is not low");
+		return EINVAL;
+	}
+	gpio_pin_set_dt(&PHO, 1);
+	k_sleep(K_MSEC(1));
+	if (gpio_pin_get_dt(&nFAULT) != 1)
+	{
+		shell_print(sh, "nFAULT is not high");
+		return EINVAL;
+	}
+	gpio_pin_set_dt(&PHO, 0);
+	shell_print(sh, "done");
+	return 0;
+}
+
+int cmd_test_other_io(const struct shell *sh, size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+	gpio_pin_set_dt(&comm_enable, 0);
+	shell_print(sh, "Connect COMM_ENABLE to nRESET_FAULT");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return;
+	}
+	if (gpio_pin_get_dt(&n_reset_fault) != 0)
+	{
+		shell_print(sh, "nRESET_FAULT is not low");
+		return;
+	}
+	gpio_pin_set_dt(&comm_enable, 1);
+	k_sleep(K_MSEC(1));
+	if (gpio_pin_get_dt(&n_reset_fault) != 1)
+	{
+		shell_print(sh, "nRESET_FAULT is not high");
+		return;
+	}
+	gpio_pin_set_dt(&comm_enable, 0);
+	shell_print(sh, "Connect COMM_ENABLE to nRESET_IN");
+	if (shell_wait(sh) == ETIMEDOUT)
+	{
+		return;
+	}
+	if (gpio_pin_get_dt(&n_reset_in) != 0)
+	{
+		shell_print(sh, "nRESET_IN is not low");
+		return;
+	}
+	gpio_pin_set_dt(&comm_enable, 1);
+	k_sleep(K_MSEC(1));
+	if (gpio_pin_get_dt(&n_reset_in) != 1)
+	{
+		shell_print(sh, "nRESET_IN is not high");
+		return;
+	}
+	gpio_pin_set_dt(&comm_enable, 0);
+	shell_print(sh, "done");
+	return 0;
+	
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_test,
+							   SHELL_CMD(hv_supply, NULL, "Test HV supply", cmd_test_hv_supply),
+							   SHELL_CMD(printhead_io, NULL, "Test printhead IO", cmd_test_printhead_io),
+							   SHELL_CMD(other_io, NULL, "Test other IO", cmd_test_other_io),
+							   SHELL_SUBCMD_SET_END);
+SHELL_CMD_REGISTER(test, &sub_test, "Test commands", NULL);
 
 int main(void)
 {
 	int ret;
 
+	ret = initialize_io();
+	if (ret != 0)
+	{
+		return ret;
+	}
 
-	if (!gpio_is_ready_dt(&button)) {
+	if (!gpio_is_ready_dt(&button))
+	{
 		printk("Error: button device %s is not ready\n",
-		       button.port->name);
+			   button.port->name);
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
+	if (ret != 0)
+	{
 		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button.port->name, button.pin);
+			   ret, button.port->name, button.pin);
 		return 0;
 	}
 
 	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_TO_ACTIVE);
-	if (ret != 0) {
+										  GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0)
+	{
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
+			   ret, button.port->name, button.pin);
 		return 0;
 	}
 
@@ -65,7 +531,7 @@ int main(void)
 	const struct device *printhead;
 
 	printhead = DEVICE_DT_GET(DT_NODELABEL(printhead));
-	
+
 	if (!device_is_ready(printhead))
 	{
 		LOG_ERR("Printhead not ready");
@@ -73,8 +539,6 @@ int main(void)
 	}
 
 	printer_sample_function(printhead);
-
-
 
 	return 0;
 }
